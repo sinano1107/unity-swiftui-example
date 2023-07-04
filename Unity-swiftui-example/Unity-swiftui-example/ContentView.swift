d90316bfad1921f3acde8b40b9a7b7eccacb63d6
sinano1107 <tftf161107@icloud.com>
2023-07-04 09:06:41 +0900

    Add xcode project

---
+++ Unity-swiftui-example/Unity-swiftui-example/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Unity-swiftui-example
+//
+//  Created by 長政輝 on 2023/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unity-swiftui-example/Unity-swiftui-example/Unity_swiftui_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unity_swiftui_exampleApp.swift
+//  Unity-swiftui-example
+//
+//  Created by 長政輝 on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct Unity_swiftui_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
